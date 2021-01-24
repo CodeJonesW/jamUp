32da3562b056ca0b64e77482e67dd1cc6a901b59
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2021-01-24 12:10:05 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from './global';
+import { theme } from './theme';
 
 function App() {
+  const [listOfJams, setJams] = useState(null);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/jams')
+    .then(res => res.json())
+    .then(data => {
+      setJams(data)
+    })
+
+    
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+    <>
+      <GlobalStyles />
+      <div>
+        <h1>Hello. This is burger menu tutorial</h1>
+      </div>
+    </>
+  </ThemeProvider>
   );
 }
 
