27ed5fbd3da6d88ac52b12da7c08192a402e589f
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2021-02-01 23:56:07 -0500

    edit signup/in css

--- src/components/public/SignIn.js
+++ src/components/public/SignIn.js
@@ -4,15 +4,13 @@ import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 
 
-
-
 const useStyles = makeStyles((theme) => ({
   newClass: {
-    width: "50vw",
+    width: "50%",
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    margin: "25%"
+    margin: "2%"
   },
 }));
 
@@ -30,7 +28,7 @@ export default function SignIn(props) {
     <div>
     <h1 className={classes.newClass}>JamUp</h1>
       <input ref={signInEmailInput} placeholder="Email" className={classes.newClass}/>
-      <input ref={signInPasswordInput} placeholder="password" className={classes.newClass}/>
+      <input type="password" ref={signInPasswordInput} placeholder="password" className={classes.newClass}/>
       <button onClick={handleSubmit} className={classes.newClass}>Submit </button>
       <Link to="/signup"><button className={classes.newClass}> Click here to Sign Up</button></Link>
     </div>
--- src/components/public/SignUp.js
+++ src/components/public/SignUp.js
@@ -6,11 +6,11 @@ import { Link } from 'react-router-dom';
 
 const useStyles = makeStyles((theme) => ({
   newClass: {
-    width: "50vw",
+    width: "50%",
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    margin: "20%"
+    margin: "2%"
   },
 }));
 
@@ -19,8 +19,15 @@ export default function SignUp() {
   const auth = useAuth();
   const signUpEmailInput = useRef(null);
   const signUpPasswordInput = useRef(null);
+  const signUpConfirmPasswordInput = useRef(null);
   
   const handleSubmit = (e) => {
+    if(signUpPasswordInput.current.value !== signUpConfirmPasswordInput.current.value){
+      alert("Passwords do not match")
+      signUpConfirmPasswordInput.current.focus();
+      return
+    }
+
     let user = auth.signup(signUpEmailInput.current.value, signUpPasswordInput.current.value)
     console.log(user.email)
   }
@@ -30,9 +37,10 @@ export default function SignUp() {
     <h1 className={classes.newClass}>Sign up with email and password</h1>
   
         <input className={classes.newClass} ref={signUpEmailInput} placeholder="Email"/>
-        <input className={classes.newClass}ref={signUpPasswordInput} placeholder="Password"/>
+        <input type="password" className={classes.newClass}ref={signUpPasswordInput} placeholder="Password"/>
+        <input type="password" className={classes.newClass}ref={signUpConfirmPasswordInput} placeholder="Confirm Password"/>
         <button className={classes.newClass} onClick={(e) => handleSubmit(e)}>submit</button>
-        <Link to="/signin"><button className={classes.newClass}> Already have an account?</button></Link>
+        <Link to="/"><button className={classes.newClass}> Already have an account?</button></Link>
     </div>
   );
 }
